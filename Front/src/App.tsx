feda9fe9388671e81e3a10cea0c999a1f411759c
Rogelio Benavides <roger.benavidesdelarosa@gmail.com>
2024-02-20 21:50:48 -0600

    Basic Background style

--- Front/src/App.tsx
+++ Front/src/App.tsx
@@ -1,34 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import './App.css'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const retrievedColors = "[\"#4285F4\", \"#34A853\", \"#FBBC05\", \"#EA4335\"]";
+  const colors = JSON.parse(retrievedColors);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Row style={{margin: '0'}}>
+      {colors.map((color: string, index: number) => (
+        <Col key={index} style={{ backgroundColor: color, height: '100vh' }}/>
+      ))}
+    </Row>
   )
 }
 
