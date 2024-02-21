604b9c4b360a7f0826b214cef8bca8b39b658810
Rogelio Benavides <roger.benavidesdelarosa@gmail.com>
2024-02-21 00:14:31 -0600

    Project Finished, Refactoring pending

--- Front/src/App.tsx
+++ Front/src/App.tsx
@@ -1,41 +1,60 @@
 import { Row, Col, Form, InputGroup, Button } from "react-bootstrap";
 import { sendRequest } from "./Services/sendRequest";
-import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
-  const retrievedColors = '["#4285F4", "#34A853", "#FBBC05", "#EA4335"]';
-  const colors = JSON.parse(retrievedColors);
+  const [retrievedColors, setReceivedColors] = useState(
+    '["#4285F4", "#34A853", "#FBBC05", "#EA4335"]'
+  );
+  const [colors, setColors] = useState(JSON.parse(retrievedColors));
 
   const formHandler = async (event: any) => {
     event.preventDefault();
     const response = await sendRequest(event.target[0].value);
+    setReceivedColors(response.colors);
   };
 
+  useEffect(() => {
+    setColors(JSON.parse(retrievedColors));
+  }, [retrievedColors]);
+
   return (
-    <div>
-      <Row style={{ margin: "0", position: "absolute", width: "100%" }}>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Row style={{ margin: "0", width: "100%" }}>
         {colors.map((color: string, index: number) => (
           <Col
             key={index}
-            style={{ backgroundColor: color, height: "100vh" }}
-          />
+            style={{
+              backgroundColor: color,
+              height: "100vh",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "end",
+            }}
+            onClick={() => {
+              navigator.clipboard.writeText(color);
+              console.log("Copied to clipboard: ", color);
+            }}
+          >
+            <h1>{color}</h1>
+          </Col>
         ))}
       </Row>
       <Form
         onSubmit={formHandler}
         style={{
-          width: "100%",
-          height: "100vh",
-          position: "absolute",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
+          position: "fixed",
+          height: "50px",
         }}
       >
-        <InputGroup className="mb-3" style={{ width: '30%'}}>
-          <Form.Control
-            placeholder="Color Palette Description"
-          />
+        <InputGroup className="mb-3">
+          <Form.Control placeholder="Color Palette Description" />
           <Button variant="primary" id="button-addon2" type="submit">
             Generate
           </Button>
--- Front/src/Services/sendRequest.ts
+++ Front/src/Services/sendRequest.ts
@@ -13,7 +13,6 @@ export const sendRequest = async (text: string) => {
         },
       }
     );
-    console.log(response.data);
     return response.data;
   } catch (error) {
     console.log(error);
